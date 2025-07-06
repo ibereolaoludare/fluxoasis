900dd3df6e1777b964b183a402be9a38a8aa179f
Ibereola Oludare <96877735+ibereolaoludare@users.noreply.github.com>
2025-07-06 17:33:55 +0100

    ui - fixed a routing problem
    wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,65 @@
-import { Route, Switch } from "wouter";
-import NotFound from "./pages/NotFound";
+import { Fragment } from "react";
+import {
+  Route as WouterRoute,
+  Switch,
+  RouteProps,
+  DefaultParams,
+  PathPattern,
+} from "wouter";
+
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
+import NotFound from "./pages/NotFound";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const urlPaths = ["home", "shop", "about", "contact"] as const; // update this array to add more url paths
+
 function App() {
-    return (
-        <Switch>
-            <Route component={Home} path={"/"} />
-            <Route component={Home} path={"/home"} />
-            <Route component={Shop} path={"/shop"} />
-            <Route component={About} path={"/about"} />
-            <Route component={Contact} path={"/contact"} />
-            <Route component={NotFound} />
-        </Switch>
-    );
+  return (
+    <Fragment>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/home" component={Home} />
+        <Route path="/shop" component={Shop} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <WouterRoute component={NotFound} />
+      </Switch>
+    </Fragment>
+  );
+}
+
+export type urlPathType = (typeof urlPaths)[number] | "";
+
+function withBasePath(route: `/${urlPathType}` = "/") {
+  const base_url = import.meta.env.BASE_URL;
+  return `${base_url.slice(0, base_url.length)}${route}`;
+}
+
+function Route<
+  T extends DefaultParams | undefined = undefined,
+  RoutePath extends PathPattern = PathPattern
+>({
+  path,
+  component,
+  ...props
+}: Omit<
+  RouteProps<T, RoutePath>,
+  keyof {
+    path: `/${urlPathType}`;
+  }
+> & {
+  path: `/${urlPathType}`;
+}) {
+  return (
+    <WouterRoute
+      path={withBasePath(path) as RoutePath}
+      component={component}
+      {...props}
+    />
+  );
 }
 
 export default App;
